541c21b86b13a60b162c29d4801bdfba06f3d909
p0tpan <bpare003@gmail.com>
2020-11-06 12:12:17 -0500

    update the lsImages function to scan for files within the ./images dir

--- main.go
+++ main.go
@@ -9,23 +9,31 @@ import (
 	"github.com/qeesung/image2ascii/convert"
 	"io/ioutil"
 	"log"
+	"path/filepath"
 )
 
-func lsImages()  {
+func lsImages() []string {
 	files, err := ioutil.ReadDir("./images")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(files[0].Name())
+
+	var listOfFiles []string
+
+	for _, file := range files {
+		listOfFiles = append(listOfFiles, file.Name())
+	}
+	return listOfFiles
 }
 
 func assRandomizer() string {
-	assList := []string{"./images/ass2.jpg", "./images/2487pgaxqaw51.jpg", "./7CwaBKErbKumOQ5YT3nQzCHSQMCHbjKYxDg-YFxOS2E.jpg", "./b_8v6vTWWDFAtPThzp_mFs8H5Yr-ArYQZfLZbuffI4I.png", "./cmwaqatpz0w51.jpg", "./CvqjdJ7pDBm5VuZ0vR-fw-N2iEvopw9OX7mOXDGv8dw.png", "./d46ossbim4v51.jpg", "./l2nbyfih2nw51.jpg", "./lrmtjoka7bo51.jpg", "./mf0kyq6r01851.jpg", "./p7pke6lwotv51.jpg", "./r9s0so5k7qw51.jpg", "./WjYU5lmgtoCS-x2rDcYlsfXvSH1DjK6T97fIzQj9K_8.png", "./y8mh1dmlyew51.jpg", "./yh1022ntbpw51.jpg", "./zscpi0hgikv51.jpg"}
-	rand.Shuffle(len(assList), func(i, j int) {
-		assList[i], assList[j] = assList[j], assList[i]
+	//assList := []string{"./images/ass2.jpg", "./images/2487pgaxqaw51.jpg", "./7CwaBKErbKumOQ5YT3nQzCHSQMCHbjKYxDg-YFxOS2E.jpg", "./b_8v6vTWWDFAtPThzp_mFs8H5Yr-ArYQZfLZbuffI4I.png", "./cmwaqatpz0w51.jpg", "./CvqjdJ7pDBm5VuZ0vR-fw-N2iEvopw9OX7mOXDGv8dw.png", "./d46ossbim4v51.jpg", "./l2nbyfih2nw51.jpg", "./lrmtjoka7bo51.jpg", "./mf0kyq6r01851.jpg", "./p7pke6lwotv51.jpg", "./r9s0so5k7qw51.jpg", "./WjYU5lmgtoCS-x2rDcYlsfXvSH1DjK6T97fIzQj9K_8.png", "./y8mh1dmlyew51.jpg", "./yh1022ntbpw51.jpg", "./zscpi0hgikv51.jpg"}
+	listOfFiles := lsImages()
+	rand.Shuffle(len(listOfFiles), func(i, j int) {
+		listOfFiles[i], listOfFiles[j] = listOfFiles[j], listOfFiles[i]
 	})
-	fmt.Println(assList[0])
-	return assList[0]
+	fmt.Println(listOfFiles[0])
+	return filepath.Join("./images/", listOfFiles[0])
 }
 
 func assPrinter() {
@@ -33,7 +41,7 @@ func assPrinter() {
 	convertOptions := convert.DefaultOptions
 	convertOptions.FixedWidth = 100
 	convertOptions.FixedHeight = 40
-	convertOptions.Colored = false
+	convertOptions.Colored = true
 
 	// Create the image converter
 	converter := convert.NewImageConverter()
@@ -43,6 +51,5 @@ func assPrinter() {
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	//assPrinter()
-	lsImages()
+	assPrinter()
 }

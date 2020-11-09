e27e95b3d34c1ee4f9526c47d288a12dd480015d
p0tpan <bpare003@gmail.com>
2020-11-08 23:39:00 -0500

    update main.go to include comments explaining the functions

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 )
 
 func lsImages() []string {
+	//a function that will scan the ./images directory and will list them within a slice of strings.
 	files, err := ioutil.ReadDir("./images")
 	if err != nil {
 		log.Fatal(err)
@@ -27,12 +28,11 @@ func lsImages() []string {
 }
 
 func assRandomizer() string {
-	//assList := []string{"./images/ass2.jpg", "./images/2487pgaxqaw51.jpg", "./7CwaBKErbKumOQ5YT3nQzCHSQMCHbjKYxDg-YFxOS2E.jpg", "./b_8v6vTWWDFAtPThzp_mFs8H5Yr-ArYQZfLZbuffI4I.png", "./cmwaqatpz0w51.jpg", "./CvqjdJ7pDBm5VuZ0vR-fw-N2iEvopw9OX7mOXDGv8dw.png", "./d46ossbim4v51.jpg", "./l2nbyfih2nw51.jpg", "./lrmtjoka7bo51.jpg", "./mf0kyq6r01851.jpg", "./p7pke6lwotv51.jpg", "./r9s0so5k7qw51.jpg", "./WjYU5lmgtoCS-x2rDcYlsfXvSH1DjK6T97fIzQj9K_8.png", "./y8mh1dmlyew51.jpg", "./yh1022ntbpw51.jpg", "./zscpi0hgikv51.jpg"}
+	//a function that randomizes the listOfFiles slice and returns the first index of that randomized slice.
 	listOfFiles := lsImages()
 	rand.Shuffle(len(listOfFiles), func(i, j int) {
 		listOfFiles[i], listOfFiles[j] = listOfFiles[j], listOfFiles[i]
 	})
-	fmt.Println(listOfFiles[0])
 	return filepath.Join("./images/", listOfFiles[0])
 }
 
